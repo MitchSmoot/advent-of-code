36e140b466fa0935924678c9bc21a5d81435edea
MitchSmoot <mitchsmoot@gmail.com>
2024-12-07 23:26:29 -0700

    working: day 8

---
+++ 2024/8/solution.js
@@ -0,0 +1,15 @@
+
+
+
+// iterate over array to create a list of each character used
+//
+// Repeat for each character used:
+// 1: create table of antinodes and their coordinates
+// 2: Check which antinodes are within bounds
+// 3: add to total
+//
+// functions needed:
+// 1: character list
+// 2: antinode position calculation
+// coordinates thing
+// 3: antinode bound checker
